402603c9e02016bf92dbb620c6f6aa1e1a7c55b3
karlo <kolboch@gmail.com>
2018-09-06 11:06:06 +0200

    Remote

---
+++ ARCoreMovement/Assets/ARCore/Scripts/CounterController.cs
@@ -0,0 +1,64 @@
+﻿using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterController : MonoBehaviour {
+
+    public GameObject SearchingForPlaneUI;
+    public GameObject CountUI;
+    public PlaceAndCount InputHandler;
+    private List<DetectedPlane> AllDetectedPlanes;
+    private bool _IsQuitting = false;
+
+    void Start() {
+        AllDetectedPlanes = new List<DetectedPlane>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        _UpdateApplicationLifeCycle();
+
+        Session.GetTrackables<DetectedPlane>(AllDetectedPlanes);
+        bool showSearchingUI = !AllDetectedPlanes.Exists(plane => plane.TrackingState == TrackingState.Tracking);
+        SearchingForPlaneUI.SetActive(showSearchingUI);
+
+        InputHandler.HandleInput();
+    }
+
+    private void _UpdateApplicationLifeCycle() {
+        // exit app on back pressed
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking) {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        } else {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+        if (_IsQuitting) { return; }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted) {
+            AndroidUtils.ShowAndroidToastMessage("Camera permission is needed to run this application");
+            _IsQuitting = true;
+            StartCoroutine(_DoQuitWithDelay(0.5f));
+        } else if (Session.Status.IsError()) {
+            AndroidUtils.ShowAndroidToastMessage("ARCore connecting problem. Please start the app again.");
+            _IsQuitting = true;
+            StartCoroutine(_DoQuitWithDelay(0.5f));
+        }
+    }
+
+    private IEnumerator _DoQuitWithDelay(float delay) {
+        yield return new WaitForSeconds(delay);
+    }
+
+    // exits the application
+    private void _DoQuit() {
+        Application.Quit();
+    }
+}
+
---
+++ ARCoreMovement/Assets/ARCore/Scripts/PlaceAndCount.cs
@@ -0,0 +1,55 @@
+﻿using GoogleARCore;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlaceAndCount : MonoBehaviour {
+
+    public Camera FirstPersonCamera;
+    public GameObject PrefabToPlace;
+    private List<Object> TrackedObjects;
+    private float k_ModelRotation = 180.0f;
+
+    public void HandleInput() {
+        Touch touch;
+        if (Input.touchCount < 1 || Input.GetTouch(0).phase != TouchPhase.Began) {
+            return;
+        }
+        touch = Input.GetTouch(0);
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit)) {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0) {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            } else {
+                // Choose the Andy model for the Trackable that got hit.
+                GameObject prefab;
+                if (hit.Trackable is FeaturePoint) {
+                    prefab = PrefabToPlace;
+                } else {
+                    prefab = PrefabToPlace;
+                }
+
+                // Instantiate Andy model at the hit pose.
+                var andyObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
+                andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+                // world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make Andy model a child of the anchor.
+                andyObject.transform.parent = anchor.transform;
+            }
+        }
+    }
+}
