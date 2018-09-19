62d891e79430804c7ab8b708b2ac4268ba3f8d0b
karlo <kolboch@gmail.com>
2018-09-19 17:14:05 +0200

    Transform around and UI

--- Quaternions/Assets/BoxRotator.cs
+++ Quaternions/Assets/BoxRotator.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxRotator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform Cube;
+    public Vector3 Origin = new Vector3(0, 0.5f, 0);
+    private bool rotateAroundOn = false;
+
+    public void ResetPosition() {
+        Cube.transform.position = Origin;
+    }
+
+    public void RotateAroundToggle() {
+        if (rotateAroundOn) {
+            rotateAroundOn = false;
+        } else {
+            rotateAroundOn = true;
+            StartCoroutine(RotateAround());
+        }
+    }
+
+    IEnumerator RotateAround() {
+        Vector3 RotationPoint = Origin + new Vector3(1, 0, 1);
+        while (true) {
+            Cube.RotateAround(RotationPoint, Vector3.up, 180 * Time.deltaTime);
+            if (rotateAroundOn) {
+                yield return null;
+            } else {
+                yield break;
+            }
+        }
+    }
 }
