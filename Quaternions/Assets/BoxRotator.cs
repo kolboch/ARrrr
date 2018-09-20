ea08cfe8f632ba3cf81068515d099e42ff20818a
karlo <kolboch@gmail.com>
2018-09-20 21:23:44 +0200

    Add fromToRotation example and improve scene

--- Quaternions/Assets/BoxRotator.cs
+++ Quaternions/Assets/BoxRotator.cs
@@ -5,11 +5,17 @@ public class BoxRotator : MonoBehaviour {
 
     public Transform Cube;
     public Transform Cyllinder;
-    public Vector3 Origin = new Vector3(0, 0.5f, 0);
+    public Vector3 Origin = new Vector3(0, 1.5f, 0);
+    public Vector3 OriginRotation = new Vector3(0, 0, 0);
     private bool rotateAroundOn = false;
+
+    private void Update() {
+        
+    }
 
     public void ResetPosition() {
         Cube.transform.position = Origin;
+        Cube.transform.rotation = Quaternion.Euler(OriginRotation);
     }
 
     public void RotateAroundToggle() {
@@ -22,8 +28,11 @@ public class BoxRotator : MonoBehaviour {
     }
 
     public void LookRotation() {
-        Vector3 relativePosition = Cyllinder.transform.position - Cube.transform.position;
-        Cube.transform.rotation = Quaternion.LookRotation(relativePosition);
+        StartCoroutine(LookRotationCoroutine());
+    }
+
+    public void FromToRotatation() {
+        Cube.transform.rotation = Quaternion.FromToRotation(Vector3.up, Cube.transform.up);
     }
 
     IEnumerator RotateAround() {
@@ -36,5 +45,16 @@ public class BoxRotator : MonoBehaviour {
                 yield break;
             }
         }
+    }
+
+    private IEnumerator LookRotationCoroutine() {
+        // forward z axis will point into provided direction
+        Vector3 relativePosition = Cyllinder.transform.position - Cube.transform.position;
+        relativePosition.y = 0;
+        Cube.transform.rotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+        yield return new WaitForSeconds(3);
+        Cube.transform.rotation = Quaternion.LookRotation(relativePosition, Vector3.left);
+        yield return new WaitForSeconds(3);
+        Cube.transform.rotation = Quaternion.LookRotation(relativePosition, Vector3.right);
     }
 }
