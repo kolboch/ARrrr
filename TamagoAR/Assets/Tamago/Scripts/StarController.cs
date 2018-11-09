7a359d2d580ed2ebab31dfa9fb75a700b782ae6e
karlo <kolboch@gmail.com>
2018-11-09 01:50:49 +0100

    Add star autodestroy and fix star icon in horizontal UI

--- TamagoAR/Assets/Tamago/Scripts/StarController.cs
+++ TamagoAR/Assets/Tamago/Scripts/StarController.cs
@@ -1,12 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class StarController : MonoBehaviour {
+
+    public float minDelay = 10f;
+    public float maxDelay = 30f;
+
+    private void Start() {
+        StartCoroutine(DestructAfterDelay());
+    }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag(Tags.TAG_CHARACTER)) {
             other.gameObject.GetComponent<AguController>().OnStarCollected();
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator DestructAfterDelay() {
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+        Destroy(gameObject);
     }
 
 }
